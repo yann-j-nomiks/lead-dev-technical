bba4f8824329cb7da1a8daf1d5b412e8d85d0ed0
yann-j-nomiks <105724620+yann-j-nomiks@users.noreply.github.com>
2022-12-08 09:43:43 +0100

    Add files via upload

---
+++ dataServices.js
@@ -0,0 +1,45 @@
+class DataServices {
+
+    _getSeries(start, end, step) {
+        const series = [];
+
+        if (start !== 0) {
+            series.push(0)
+        }
+        let i;
+        for (i = start; i <= end; i += step)
+            series.push(i);
+
+        console.log(i,end)
+        if (i !== end + step )
+            series.push(end);
+
+        return series;
+    }
+
+    sigmoidSeries(start, end, step, mean, deviation, totalValue) {
+
+        step = Math.floor((end-start) / 10)   ;
+        const sigmoid = (x) => {
+            if (x <= 0)
+                return 0;
+            if(x===end-start)
+                return totalValue;
+
+            return 1 / (1 + Math.exp(-mean * (x - deviation))) * totalValue;
+        }
+
+        const series = this._getSeries(start, end, step);
+        let sigmoidSeries = [];
+        series.forEach((x) => {
+            sigmoidSeries.push({x, value: sigmoid(x - start)});
+        });
+        console.log(sigmoidSeries)
+        return sigmoidSeries;
+    }
+
+}
+
+
+const dataService = new DataServices();
+export {dataService}
